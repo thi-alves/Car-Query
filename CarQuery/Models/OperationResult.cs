db6e0ebdec91c5c06b3268e713e017ab00733f07
Thiago de Oliveira Alves <thiago.oalves246@gmail.com>
2025-03-09 15:20:13 -0300

    Feature: Add OperationResult class for operation status handling

---
+++ CarQuery/Models/OperationResult.cs
@@ -0,0 +1,15 @@
+﻿namespace CarQuery.Models
+{
+    public class OperationResult
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+
+        public OperationResult() { }
+
+        public OperationResult(bool succeeded, string message) {
+            Succeeded = succeeded;
+            Message = message;
+        }
+    }
+}
