45b047a516f7b8949a305a2c0f901248f86cb7bf
thi-alves <thiago.oalves246@gmail.com>
2025-05-21 11:29:26 -0300

    feat: add new area SuperAdmin and UserManagement controller

---
+++ CarQuery/Areas/SuperAdmin/Controllers/UserManagementController.cs
@@ -0,0 +1,26 @@
+﻿using CarQuery.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReflectionIT.Mvc.Paging;
+
+namespace CarQuery.Areas.SuperAdmin.Controllers
+{
+    [Area("SuperAdmin")]
+    [Authorize(Roles = "SuperAdmin")]
+    public class UserManagementController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public UserManagementController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, AppDbContext context)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _context = context;
+        }
+    }
+
+}
