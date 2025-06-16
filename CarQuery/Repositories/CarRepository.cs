a72c3d78ddb9f51f7911b32652321fffdaecb4a4
thi-alves <thiago.oalves246@gmail.com>
2025-06-15 23:09:25 -0300

    fix: delete car images from server and simplify CarRepository code

--- CarQuery/Repositories/CarRepository.cs
+++ CarQuery/Repositories/CarRepository.cs
@@ -1,27 +1,26 @@
-﻿using System.Linq.Expressions;
-using CarQuery.Data;
+﻿using CarQuery.Data;
 using CarQuery.Models;
 using CarQuery.Repositories.Interface;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarQuery.Repositories
 {
     public class CarRepository : ICarRepository
     {
-
+        private string ServerPath { get; set; }
         private readonly AppDbContext _context;
 
-        public CarRepository(AppDbContext context)
+        public CarRepository(IWebHostEnvironment system, AppDbContext context)
         {
+            ServerPath = system.WebRootPath;
             _context = context;
         }
 
         public async Task<bool> AddCar(Car car)
         {
             await _context.Car.AddAsync(car);
-            int rowsAffected = await _context.SaveChangesAsync();
-            return rowsAffected > 0;
+            await _context.SaveChangesAsync();
+            return true;
         }
 
         public async Task<Car> GetCarById(int carId)
@@ -40,8 +39,8 @@ namespace CarQuery.Repositories
                 return false;
             }
             _context.Update(car);
-            int rowsAffected = await _context.SaveChangesAsync();
-            return rowsAffected > 0;
+            await _context.SaveChangesAsync();
+            return true;
         }
 
         public async Task<bool> DeleteCar(int carId)
@@ -52,26 +51,39 @@ namespace CarQuery.Repositories
 
             if (car != null)
             {
+                foreach (var img in car.Images)
+                {
+                    string normalizedPath = (img.ImgPath).Replace("/", "\\");
+                    string imgPath = ServerPath + normalizedPath;
+
+                    if (System.IO.File.Exists(imgPath))
+                    {
+                        //deletando a imagem do servidor
+                        System.IO.File.Delete(imgPath);
+                    }
+                }
                 _context.Car.Remove(car);
 
-                int rowsAffected = await _context.SaveChangesAsync();
-                return rowsAffected > 0;
+                await _context.SaveChangesAsync();
+                return true;
             }
             return false;
         }
 
         public async Task<IEnumerable<Car>> SearchByModel(string model)
         {
-
-            if (!string.IsNullOrEmpty(model))
+            if (string.IsNullOrEmpty(model))
             {
-                var car = await _context.Car.Include(c => c.Images)
-                .Where(c => c.Model.StartsWith(model))
-                .ToListAsync();
-
-                if (car != null) return car;
+                return Enumerable.Empty<Car>();
             }
-            return Enumerable.Empty<Car>();
+
+            var car = await _context.Car.Include(c => c.Images)
+            .Where(c => c.Model.StartsWith(model))
+            .ToListAsync();
+
+            return car;
         }
+
     }
 }
+
