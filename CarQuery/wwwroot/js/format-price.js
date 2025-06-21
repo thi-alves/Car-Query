627f7330f8d211cff1b0ebd55121166da9fc6746
thi-alves <thiago.oalves246@gmail.com>
2025-06-21 20:06:47 -0300

    feat: add real-time price formatting and move it to individual file

---
+++ CarQuery/wwwroot/js/format-price.js
@@ -0,0 +1,42 @@
+﻿const formatedPriceInput = document.getElementById("formatedPriceInput");
+const realPriceInput = document.getElementById("realPriceInput");
+
+document.addEventListener("DOMContentLoaded", function () {
+    formatInitialPrice();
+});
+
+formatedPriceInput.addEventListener('input', function () {
+	let value = this.value.replace(/\D/g, '');
+
+	if (value === '') {
+		this.value = '';
+		realPriceInput.value = 0;
+		return;
+	}
+
+	let numericValue = parseFloat(value) / 100;
+
+	this.value = numericValue.toLocaleString('pt-br', {
+		style: 'currency',
+		currency: 'BRL'
+	});
+
+	realPriceInput.value = numericValue;
+});
+
+function formatInitialPrice() {
+	const initialPrice = realPriceInput.value;
+
+	let numericValue = parseFloat(initialPrice);
+
+	if (isNaN(numericValue)) {
+		numericValue = 0;
+	}
+	const value = numericValue.toLocaleString('pt-br', {
+		style: 'currency',
+		currency: 'BRL'
+	});
+
+	formatedPriceInput.value = value;
+	realPriceInput.value = numericValue
+}
