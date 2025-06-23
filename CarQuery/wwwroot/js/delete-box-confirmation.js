26b254e87cd414cb970596ba4516e6df820738eb
thi-alves <thiago.oalves246@gmail.com>
2025-06-23 18:47:30 -0300

    refactor: extract delete confirmation box html to partial view, and its script to new JS file

---
+++ CarQuery/wwwroot/js/delete-box-confirmation.js
@@ -0,0 +1,10 @@
+﻿
+//target é o atributo usado para identificar o item a ser deletado
+function openConfirmBox(target) {
+	document.getElementById("target").value = target;
+	document.getElementById("customConfirmBox").style.display = "flex";
+}
+
+function closeConfirmBox() {
+	document.getElementById("customConfirmBox").style.display = "none";
+}
